2ef8475078aaae54cb17c4ea582a1e62cd2f0095
Tim Quelch <tim@tquelch.com>
2020-10-19 14:11:06 +1100

    Extract more data from commits

--- Timeline.jl
+++ Timeline.jl
@@ -3,6 +3,7 @@ module NotesTimeline
 using LibGit2: GitCommit, GitRepo, GitRevWalker, GitTree, GitTreeEntry, GitBlob, with, filename, peel, treewalk, entrytype, isbinary, content
 using LibGit2
 using DataFrames
+using Dates
 using Lazy
 
 const EXCLUDE_FILES = Set(["inbox", "todo", "setup"])
@@ -39,16 +40,25 @@ end
 
 function processCommit(id, repo)
     # @info "Processing commit" id repo
-    contents = getFileContents(GitCommit(repo, id))
+    c = GitCommit(repo, id)
+    sig = LibGit2.author(c)
+    time = unix2datetime(sig.time + sig.time_offset*60)
+    contents = getFileContents(c)
     # @info "blobs" blobs
-    return mapreduce(length, +, contents)
+    return (
+        date=time,
+        words=mapreduce(f -> length(split(f)), +, contents),
+        files=length(contents),
+        commit=sprint(print, LibGit2.GitHash(c)),
+    )
 end
 
 function processRepo(path)
     r = GitRepo(path)
     @info "Created repo" r
-    commitdata = with(walker -> LibGit2.map(processCommit, walker), GitRevWalker(r))
-
+    data = with(walker -> LibGit2.map(processCommit, walker), GitRevWalker(r)) |> DataFrame
+    @info "data" data
+    return data
 end
 
 end
